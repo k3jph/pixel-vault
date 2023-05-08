0aab1efcc07ffed485e2f7ade91c524b6dd00f73
James P. Howard, II <jh@jameshoward.us>
2023-05-08 19:11:31 -0400

    Updated Truffle config from VK

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,25 @@
 const path = require("path");
+
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { INFURA_API_KEY, MNEMONIC } = process.env;
+
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 9545,
+      port: 7545,
       network_id: "*"
     },
+ 
     dashboard: {
       port: 24012
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
+      network_id: '11155111',
+      gas: 4465030
     }
   },
   mocha: {},
